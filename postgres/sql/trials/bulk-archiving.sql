353ac7dd7fe0b12db7f21c4affbd462dc4480125
upamanyu <uchakravarty@ti.com>
2024-11-07 02:32:44 +0530

    refactor: limits per loop added to bulk archiving

--- postgres/sql/trials/bulk-archiving.sql
+++ postgres/sql/trials/bulk-archiving.sql
@@ -30,7 +30,7 @@ create table myschema.archived_items (
 insert into myschema.list (list_id, list_name, deleted_ts)
 	select 1, 'List 1', null::date
 	union
-	select 2, 'List 2', null::date
+	select 2, 'List 2', current_date::date
 	union
 	select 3, 'List 3', current_date::date
 	union
@@ -50,7 +50,7 @@ insert into myschema.items (item_id, list_id, part_number, qty)
 	union
 	select 6, 5, 'TPA6', 8;
 
--- actual archiving job
+-- archiving job without limits per loop
 do $$
 declare
 	list_ids int[];
@@ -83,14 +83,65 @@ begin
 		
 end $$;
 
+-- archiving job with limits per loop
+do $$
+declare
+	loop_limit int = 2;
+	loop_counter int = 0;
+	list_ids int[];
+begin
+loop
+
+	-- increment counter and log
+	loop_counter = loop_counter + 1;
+	raise info 'loop count = %', loop_counter;
+
+	-- get first x lists to be deleted
+	list_ids = array(
+		select list_id from myschema.list where deleted_ts is not null limit loop_limit
+	);
+
+	-- if it was 1000 at a time, then we can loop through until list_ids is empty
+	if array_length(list_ids, 1) > 0 then
+
+		-- copy lists
+		insert into myschema.archived_list (list_id, list_name, deleted_ts, archived_ts)
+			select list_id, list_name, deleted_ts, current_date from myschema.list
+			where list_id = any(list_ids);
+		
+		-- delete and insert items in one go
+		with archived_items as (
+			delete from myschema.items
+			where list_id = any(list_ids)
+			returning *
+		) insert into myschema.archived_items
+			select * from archived_items;
+		
+		-- delete lists
+		delete from myschema.list where list_id = any(list_ids);
+
+	else
+		raise info 'no lists to archive';
+		exit;
+	end if;
+
+end loop;
+end $$;
+
 -- select
 select* from myschema.list;
 select* from myschema.items;
 select* from myschema.archived_list;
 select* from myschema.archived_items;
 
+-- temp cleanup
+truncate table myschema.items;
+delete from myschema.list;
+truncate table myschema.archived_list;
+truncate table myschema.archived_items;
+
 -- cleanup
-drop table myschema.list;
 drop table myschema.items;
+drop table myschema.list;
 drop table myschema.archived_list;
 drop table myschema.archived_items;
